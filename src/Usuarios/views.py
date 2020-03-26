59692d740ee99f00aab7a793497e1b7e0e567fcd
Jorge Armi <jgarmi95@gmail.com>
2020-03-26 15:20:12 +0100

    lectura de datos del models, desde el views

--- src/Usuarios/views.py
+++ src/Usuarios/views.py
@@ -7,52 +7,27 @@ from .models import Usuarios
 from .forms import UsuariosForm, InfoForm
 from django.views.generic import ListView, TemplateView
 
+from django.template import Template, Context
+
+
 # Create your views here.
 def inicio(request): 
 	if request.user.is_authenticated:
-		titulo = "Bienvenid@! %s" %(request.user) #"Bienvenido %s" %(request.user)
+		titulo = "Bienvenid@! %s" %(request.user) 
 
 	form = UsuariosForm(request.POST or None)
 
-	context = {
-		#"titulo" : titulo,
-		"el_form": form,
-	}   
+	context = {"el_form": form,}   
 	if form.is_valid():
 		instance = form.save(commit=False)
 		if not instance.nombre:
 			instance.nombre = "Usuario sin nombre"
 		instance.save()
-		context = {
-			"titulo" : "Gracias %(nombre)!" 
-		}
-		context = {
-			"titulo" : "Gracias por registrate en nuestra aplicación."	 
-		}
-
 		print (instance)
 		
 	return render(request,"inicio.html", context)
 
 def category(request):
-    form = InfoForm(request.GET or None)
-
-    context = {'object_list': usuarios_lista}
+    queryset = Usuarios.objects.all()
+    context = {'queryset': queryset,}
     return render(request,'info.html', context)
-
-
-def info(ListView):
-	template_name = "info.html"
-	model = Usuarios()
-	nombres = Usuarios.objects.all()
-	context_object_name = "nombres"
-
-	return HttpResponse(nombres,"info.html")
-
-def prueba(ListView):
-	template_name = "info.html"
-	model = Usuarios()
-	queryset = Usuarios.objects.all()
-	context_object_name = "usuarios_lista"
-
-	return HttpResponse(Usuarios)
