219f842e40072309c21b6bcd51dcdea3e2d65b0a
Jorge Armi <jgarmi95@gmail.com>
2020-03-27 11:17:02 +0100

    Modificaciones internas de ficheros

--- src/data_jars/models.py
+++ src/data_jars/models.py
@@ -19,15 +19,15 @@ class Data(models.Model):
 class Uuaa(models.Model):
 
 	UNIDADES_APILCATIVAS = ["EBCF","EYCX","EYPH","KCMX","KYFJ","KYFX","KYNO","KYTA","ERGS","EYOZ","ETNC","KYFB","KYGU","KYHD","KYNF","KYOP","KYOS","KYNB"]
-	uuaas = models.MultipleChoiceField(max_length=10, blank=False, null=False, verbose_name="UUAA", choices=UNIDADES_APLICATIVAS)
+	uuaa = models.MultipleChoiceField(max_length=10, blank=False, null=False, verbose_name="UUAA", choices=UNIDADES_APLICATIVAS)
 	tecnologia = models.CharField(max_length=50, blank=True, null=False)
 	data = models.ManyToManyFields(Data)
 
 	ear = models.CharField(max_length=100, blank=False, null= False)
 	war = models.CharField(max_length=100, blank=False, null= False)
 
-	def get_default_routes(self):
-		return self.first_route + "" + self.second_route
+	def get_full_route(self):
+		return self.ear + "" + self.war
 
 	def __str__(self):
 		return uuaa
